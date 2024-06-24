209943af3b41bd111ba082a790108772958c515e
gipapa <gipapa.cs94@g2.nctu.edu.tw>
2024-06-24 15:23:34 +0800

    fix all page

--- src/main.py
+++ src/main.py
@@ -6,21 +6,14 @@ from langchain_groq import ChatGroq
 
 
 response = False
-prompt_tokens = 0
-completion_tokes = 0
-total_tokens_used = 0
-cost_of_response = 0
 
-
-
-
-st.header("Streamlit plus OpenAI ChatGPT/GPT3.5 turbo")
+st.header("Streamlit plus groq")
 
 st.markdown("""---""")
 
 question_input = st.text_input("Enter question/prompt! 輸入問題/提示！")
-api_key_input = st.text_input("Enter Your OPENAI api key! 輸入你的OPENAI api key！")
-rerun_button = st.button("Rerun 運行")
+#api_key_input = st.text_input("Enter Your OPENAI api key! 輸入你的OPENAI api key！")
+rerun_button = st.button("run")
 
 st.markdown("""---""")
 
@@ -29,14 +22,6 @@ st.markdown("""---""")
 openai.api_key = api_key_input
 
 
-def make_request_backup(question_input: str):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": f"{question_input}"},
-        ]
-    )
-    return response
 
 def make_request(question_input: str):    
     chat = ChatGroq(temperature=0.5, model="llama3-70b-8192", api_key='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')
@@ -46,10 +31,6 @@ def make_request(question_input: str):
     chain = prompt | chat
     return chain.invoke({"text": question_input}).content     
     
-
-
-
-
 
 if question_input:
     response = make_request(question_input)
@@ -64,23 +45,3 @@ else:
 if response:
     st.write("Response:")
     st.write(response)
-'''    
-    st.write(response["choices"][0]["message"]["content"])
-
-    prompt_tokens = response["usage"]["prompt_tokens"]
-    completion_tokes = response["usage"]["completion_tokens"]
-    total_tokens_used = response["usage"]["total_tokens"]
-
-    cost_of_response = total_tokens_used * 0.000002
-else:
-    pass
-
-
-with st.sidebar:
-    st.title("Usage Stats:")
-    st.markdown("""---""")
-    st.write("Promt tokens used/提示token使用量:", prompt_tokens)
-    st.write("Completion tokens used/Completion toekn使用量:", completion_tokes)
-    st.write("Total tokens used/token使用總量:", total_tokens_used)
-    st.write("Total cost of request/全部花費: ${:.8f}".format(cost_of_response))
-'''
