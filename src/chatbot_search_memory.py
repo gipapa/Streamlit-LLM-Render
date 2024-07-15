153f3238072bc4edb62aa69ea114b4d2dcc6e230
gipapa <gipapa.cs94@g2.nctu.edu.tw>
2024-07-15 17:03:38 +0800

    update RAGchatbot

--- src/chatbot_search_memory.py
+++ src/chatbot_search_memory.py
@@ -1,39 +1,77 @@
-import streamlit as st
-from decouple import config
-import openai, os
-from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import ConversationalChatAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.runnables import RunnableConfig
+#from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
+
+import streamlit as st
+import os
 
 def app():
     if 'groq_setting' not in st.session_state:
         st.error("GROQ settings not found. Please configure the settings in the main app.")
         return
-
+        
     API_KEY = st.session_state.groq_setting['API_KEY']
     MODEL = st.session_state.groq_setting['MODEL']
-    TEMPERATURE = st.session_state.groq_setting['TEMPERATURE']
-    response = False
-    
-    st.title('naive chatbot')
+    TEMPERATURE= st.session_state.groq_setting['TEMPERATURE']
+
+    st.title('Chatbot with search and momery')
+    st.write("🦜LangChain")
     st.write(f"✨groq({MODEL})")
-    st.markdown("""---""")
-    question_input = st.text_input("Enter question/prompt! 輸入問題/提示！")
-    rerun_button = st.button("run")
-    st.markdown("""---""")
-    
-    def make_request(question_input: str):    
-        chat = ChatGroq(temperature=float(TEMPERATURE), model=MODEL, api_key=API_KEY, streaming=True)
-        system = "You are a helpful assistant."
-        human = "{text}"
-        prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
-        chain = prompt | chat
-        return chain.invoke({"text": question_input}).content     
-        
-    if question_input:
-        response = make_request(question_input)
-    elif rerun_button:
-        response = make_request(question_input)
-    
-    if response:
-        st.write("Response:")
-        st.write(response)
+    st.write("🦆DuckDuckGo")
+
+
+
+    #openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+    openai_api_key = '123'
+
+    msgs = StreamlitChatMessageHistory()
+    memory = ConversationBufferMemory(
+        chat_memory=msgs, return_messages=True, memory_key="chat_history", output_key="output"
+    )
+    if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
+        msgs.clear()
+        msgs.add_ai_message("How can I help you?")
+        st.session_state.steps = {}
+
+    avatars = {"human": "user", "ai": "assistant"}
+    for idx, msg in enumerate(msgs.messages):
+        with st.chat_message(avatars[msg.type]):
+            # Render intermediate steps if any were saved
+            for step in st.session_state.steps.get(str(idx), []):
+                if step[0].tool == "_Exception":
+                    continue
+                with st.status(f"**{step[0].tool}**: {step[0].tool_input}", state="complete"):
+                    st.write(step[0].log)
+                    st.write(step[1])
+            st.write(msg.content)
+
+    if prompt := st.chat_input(placeholder="Could you tell me who won the NBA final in 2024, and who was named the FMVP?"):
+        st.chat_message("user").write(prompt)
+
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+
+        #llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
+        llm = ChatGroq(temperature=TEMPERATURE, model=MODEL, api_key=API_KEY, streaming=True)
+        tools = [DuckDuckGoSearchRun(name="Search")]
+        chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools)
+        executor = AgentExecutor.from_agent_and_tools(
+            agent=chat_agent,
+            tools=tools,
+            memory=memory,
+            return_intermediate_steps=True,
+            handle_parsing_errors=True,
+        )
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            cfg = RunnableConfig()
+            cfg["callbacks"] = [st_cb]
+            response = executor.invoke(prompt, cfg)
+            st.write(response["output"])
+            st.session_state.steps[str(len(msgs.messages) - 1)] = response["intermediate_steps"]
